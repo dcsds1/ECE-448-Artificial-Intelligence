a58365a4bac2aa441d27e815c3a2db4eb239dc9d
dcsds1 <dcsds1@sina.com>
2016-11-07 14:48:23 -0600

    finished

--- hw3/relaxed_bayes.py
+++ hw3/relaxed_bayes.py
@@ -9,9 +9,14 @@ Created on Sun Oct 30 11:06:15 2016
 import numpy as np
 import itertools
 
-smoothing_constant = 1
+k = 1 #smoothing_constant
 
 class NaiveBayes:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.all_combinations = [np.reshape(np.array(i), (n, m)) for i in itertools.product([0, 1], repeat = n*m)]
+        
     def get_images(self, data_file_name):
         """
         Build the data set
@@ -69,78 +74,105 @@ class NaiveBayes:
         for i in range(10):
             priors.append(len(np.where(label == i)[0]) / len(label))
         return np.asarray(priors)
-    
-    def likelihoods(self, train_data, train_label):
-        """
-        estimate the likelihoods P(Fij | class) for every pixel location (i,j) 
-        and for every digit class from 0 to 9
-        """
         
-        likelihoods = np.zeros((20, 28, 28))
-        for i in range(10):
-            index = np.where(train_label == i)[0]
-            data_i = np.sum(train_data[index], 0)
-            
-            # likelihoods with Laplace smoothing
-            likelihoods[2 * i] = (((len(index) - data_i) + smoothing_constant )/
-                                 (len(index) + smoothing_constant * 2)) # P(F_ij == 0 \ class)
-            likelihoods[2 * i + 1] = ((data_i + smoothing_constant) / 
-                                     (len(index) + smoothing_constant * 2)) # P(F_ij == 1 \ class)
-        return likelihoods
-        
-    def joint_likelihoods(self, train_data, train_label, n, m):
+    def joint_likelihoods(self, train_data, train_label, joint=True):
         """
         n * m feature
         """
         
-        likelihoods = np.zeros((2**(n*m)*10, 28/n, 28/m))
-        all_combinations = [np.reshape(np.array(i), (n, m)) for i in itertools.product([0, 1], repeat = n*m)]
+        if joint == True:
+            likelihoods = np.zeros((2**(self.n*self.m)*10, 28 // self.n, 28 // self.m))
+            for i in range(10):
+                class_index = np.where(train_label == i)[0]
+                data_i = train_data[class_index]
+                
+                for sample_idx in range(len(data_i)):
+                    for row in range(28 // self.n):
+                        for col in range(28 // self.m):
+                            combination_index = np.where(np.sum(self.all_combinations == 
+                                                    data_i[sample_idx][self.n * row : self.n * row + self.n, 
+                                                                       self.m * col : self.m * col + self.m],
+                                                                       axis=(1,2)) == 4)[0]
+                            likelihoods[2**(self.n*self.m) * i + combination_index[0]][row][col] += 1
+                                        
+                # smoothing
+                likelihoods[2**(self.n*self.m) * i : 2**(self.n*self.m) * (i + 1)] += k 
+                likelihoods[2**(self.n*self.m) * i : 2**(self.n*self.m) * (i + 1)] /= (len(data_i) + k * 2**(self.n*self.m))
+        # disjoint
+        else:
+            likelihoods = np.zeros((2**(self.n*self.m)*10, 29 - self.n, 29 - self.m))
+            for i in range(10):
+                class_index = np.where(train_label == i)[0]
+                data_i = train_data[class_index]
+                
+                for sample_idx in range(len(data_i)):
+                    for row in range(29 - self.n):
+                        for col in range(29 - self.m):
+                            combination_index = np.where(np.sum(self.all_combinations == 
+                                                    data_i[sample_idx][row : row + self.n, 
+                                                                       col : col + self.m],
+                                                                       axis=(1,2)) == 4)[0]
+                            likelihoods[2**(self.n*self.m) * i + combination_index[0]][row][col] += 1
+                                        
+                # smoothing
+                likelihoods[2**(self.n*self.m) * i : 2**(self.n*self.m) * (i + 1)] += k 
+                likelihoods[2**(self.n*self.m) * i : 2**(self.n*self.m) * (i + 1)] /= (len(data_i) + k * 2**(self.n*self.m))
         
-        for i in range(10):
-            index = np.where(train_label == i)[0]
-            data_i = train_data[index]
-            
-            for sample in range(len(data_i)):
-                for row in range(28 / n):
-                    for col in range(28 / m):
-                        index = np.where(np.sum(all_combinations == 
-                                         data_i[n * row : n * row + n, m * col : m * col + m],
-                                                axis=(1,2)) == 4)[0]
-                        likelihoods[2**(n*m) * i + index][row][col] += 1
-        # smoothing！！！！！！！！！！！！
         
         return likelihoods
         
-    def train(self, train_data_file, train_label_file, n, m):
+    def train(self, train_data_file, train_label_file):
         train_data = self.get_images(train_data_file)
         train_label = self.get_label(train_label_file)
         priors_matrix = self.priors(train_label)
-        likelihoods_matrix = self.likelihoods(train_data, train_label, n, m)
+        likelihoods_matrix = self.joint_likelihoods(train_data, train_label, joint=False)
         return likelihoods_matrix, priors_matrix
         
 # ================================= test ====================================
         
-    def predict_label(self, test_sample, likelihoods_matrix, priors_matrix):
+    def predict_label(self, test_sample, likelihoods, priors_matrix, joint=True):
         predict_label = np.zeros(10)
-        posterior = np.zeros((28, 28))
-        for i in range(10):
-            for j in range(28):
-                for k in range(28):
-                    posterior[j][k] = likelihoods_matrix[2 * i + test_sample[j][k]][j][k]
-            predict_label[i] = np.sum(np.log(posterior))
-        posterior_probability = predict_label + np.log(priors_matrix)
-        label = np.argmax(posterior_probability)
-        prob = posterior_probability[label]
+        if joint == True:
+            posterior = np.zeros((28 // self.n, 28 // self.m))
+            for i in range(10):
+                for row in range(28 // self.n):
+                    for col in range(28 // self.m):
+                        combination_index = np.where(np.sum(self.all_combinations == 
+                                                    test_sample[self.n * row : self.n * row + self.n,
+                                                                self.m * col : self.m * col + self.m],
+                                                    axis=(1,2)) == 4)[0]
+                        posterior[row][col] = likelihoods[2**(self.n*self.m) * i + combination_index[0]][row][col]
+                predict_label[i] = np.sum(np.log(posterior))
+            posterior_probability = predict_label + np.log(priors_matrix)
+            label = np.argmax(posterior_probability)
+            prob = posterior_probability[label]
+
+        # disjoint
+        else:
+            posterior = np.zeros((29 - self.n, 29 - self.m))
+            for i in range(10):
+                for row in range(29 - self.n):
+                    for col in range(29 - self.m):
+                        combination_index = np.where(np.sum(self.all_combinations == 
+                                                    test_sample[row : row + self.n,
+                                                                col : col + self.m],
+                                                    axis=(1,2)) == 4)[0]
+                        posterior[row][col] = likelihoods[2**(self.n*self.m) * i + combination_index[0]][row][col]
+                predict_label[i] = np.sum(np.log(posterior))
+            posterior_probability = predict_label + np.log(priors_matrix)
+            label = np.argmax(posterior_probability)
+            prob = posterior_probability[label]
+
         return label, prob
         
-    def test(self, test_data_file, test_label_file, likelihoods_matrix, priors_matrix):
+    def test(self, test_data_file, test_label_file, likelihoods_matrix, priors_matrix, joint=True):
         test_data = self.get_images(test_data_file)
         test_label = self.get_label(test_label_file)
         predict_results = []
         max_prob = np.zeros(10)
         min_prob = np.zeros(10)
         for i in range(len(test_data)):
-            label, prob = self.predict_label(test_data[i], likelihoods_matrix, priors_matrix)
+            label, prob = self.predict_label(test_data[i], likelihoods_matrix, priors_matrix, joint)
             predict_results.append(label)
             if prob > max_prob[label]:
                 max_prob[label] = prob
@@ -158,6 +190,11 @@ class NaiveBayes:
         return np.sum(predicts[:, None] == test_label) / len(test_label)
           
 if __name__ == '__main__':
-    classifier = NaiveBayes()
-    like, pri = classifier.train('trainingimages', 'traininglabels', 2, 2)
-    predict_results, test_label = classifier.test('testimages', 'testlabels', like, pri)
+    classifier = NaiveBayes(2,2)
+    
+    like, pri = classifier.train('trainingimages', 'traininglabels')
+    predict_results, test_label = classifier.test('testimages', 'testlabels', like, pri, joint=False)
+
+#    train_data = classifier.get_images('trainingimages')
+#    train_label = classifier.get_label('traininglabels')
+#    likelihoods = classifier.joint_likelihoods(train_data, train_label, 2, 2)
